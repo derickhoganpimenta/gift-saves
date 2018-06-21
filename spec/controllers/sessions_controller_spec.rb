4fa07b81e3e9aa9c9c067b00e365f273dcfcd488
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2018-06-21 15:23:14 -0300

    Change pages text

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe SessionsController, type: :controller do
   describe "#open new" do
-    it "Should be open homes#index" do
+    it "Should be open sessions#new" do
       get :new
       expect(response).to render_template("new")
     end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-
+describe UsersController, type: :controller do
+  describe "#open new" do
+    it "Should be open users#new" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
 end
